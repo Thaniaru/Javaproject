3d57a19908420dd2ac94dd1fd4ef443a03040719
thani <thani@Scooby.home>
2022-08-18 13:38:36 -0500

    updating discount on iphone

--- src/Practice/welcome.java
+++ src/Practice/welcome.java
@@ -8,6 +8,7 @@ System.out.println("Welcome to the world of DevOps");
 
 System.out.println(50+40);
 System.out.println("Hurray we did it");
+System.out.println("$100 discount on iPhone 13 Pro all variants");
 
 	}
 
