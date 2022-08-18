ca9ee03e0d59350d519876790254cce8793072ec
thani <thani@Scooby.home>
2022-08-18 13:50:04 -0500

    testing integration

--- src/Practice/welcome.java
+++ src/Practice/welcome.java
@@ -9,6 +9,7 @@ System.out.println("Welcome to the world of DevOps");
 System.out.println(50+40);
 System.out.println("Hurray we did it");
 System.out.println("$100 discount on iPhone 13 Pro all variants");
+System.out.println("Testing github and Jenkins");
 
 	}
 
