184e5a625d465f10b8beb9bf53b8283e9bdc4be6
thani <thani@Scooby.home>
2022-08-18 13:29:59 -0500

    upload programme

---
+++ src/Practice/welcome.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class welcome {
+
+	public static void main(String[] args) {
+
+System.out.println("Welcome to the world of DevOps");
+
+System.out.println(50+40);
+
+	}
+
+}
