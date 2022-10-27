5acbf749f24fcb42161f52a26f81082ab5d1f42c
thani <thani@Scooby.home>
2022-10-27 12:57:31 -0500

    update

--- src/Practice/welcome.java
+++ src/Practice/welcome.java
@@ -12,7 +12,9 @@ System.out.println("$100 discount on iPhone 13 Pro all variants");
 System.out.println("Testing github and Jenkins");
 System.out.println("$50 discount on printer tank");
 System.out.println("Welcome To all");
-System.out.println(Hi Good morning to all)
+System.out.println("good morning");
+System.out.println(100+20);
+
 
 
 	}
